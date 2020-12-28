8b03d2210b4487bed4e94b8f5f27af83e0560fdf
KhanggNguyen <kukhang2411@gmail.com>
2020-12-28 21:54:00 +0100

    maj fixtures

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PostRepository;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
