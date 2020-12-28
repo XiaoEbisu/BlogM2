8afbe7a2ae5bd855c9d8c4ca9313adc25499e42d
KhanggNguyen <kukhang2411@gmail.com>
2020-12-28 21:18:47 +0100

    maj fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
+use DateInterval;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +14,14 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $slugger = new AsciiSlugger();
+        for($i=0; $i<20; $i++){
+            $post = new Post();
+            $post->setTitle("Post " . $i);
+            $post->setUrlAlias($slugger->slug($post->getTitle()));
+            $post->setPublished((new \DateTime())->sub(new DateInterval('P'.$i.'D')));
+            $manager->persist($post);
+        }
 
         $manager->flush();
     }
