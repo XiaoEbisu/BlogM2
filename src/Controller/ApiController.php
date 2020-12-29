4dac9682d8925f25c261ff78bdaf1c435a7c3776
KhanggNguyen <kukhang2411@gmail.com>
2020-12-29 01:20:59 +0100

    ajout api index last 5 articles

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +19,22 @@ class ApiController extends AbstractController
      * @Route("/", name="api_index")
      */
     public function index(PostRepository $postRepository): Response
-    {
+    {   
+        $posts = $postRepository->findBy(array(), array('published' => 'DESC'), 5);
+        $serializedPosts = [];
+        foreach($posts as $post){
+            array_push($serializedPosts, $this->serializePost($post));
+        }
 
-        return $this->json('api/index.html.twig', [
-            'controller_name' => 'ApiController',
-        ]);
+        return new JsonResponse(['post' => $serializedPosts, 'items' => count($serializedPosts)]);
+    }
+
+    private function serializePost(Post $post){
+        return array(
+            'title' => $post->getTitle(),
+            'url_alias' => $post->getUrlAlias(),
+            'content' => $post->getContent(),
+            'published' => $post->getPublished()
+        );
     }
 }
